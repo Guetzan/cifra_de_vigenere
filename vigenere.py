ffae3d25d9b6d5c0e5ac94c89e8f62eaf2a21131
Guetzan <orcsdzn@gmail.com>
2023-10-18 01:26:51 -0300

    adicionando menu e funcionalidade de receber texto e chave do usuário

--- vigenere.py
+++ vigenere.py
@@ -1,11 +1,9 @@
 #coisas pra fazer:
-# - função de decifrar
+# - implementar função de decifrar
 # - possibilitar a encriptação de mais caracteres além de A-Z (provavelmente utilizando UNICODE)
-# - adicionar opção de usuário entrar com o texto e chave
-# - limitar tamanho do texto a ser cifrado/decifrado em 128 caracteres
+# - limitar tamanho do texto a ser cifrado/decifrado em 128 caracteres [já funcionando para a função cifrar]
 # - validar se os caracteres passados no texto estão dentro dos permitidios
-# - possibilitar o usuário realizar quantas operações desejar
-# - implementar um menu que apresenta as opções possíveis para o usuário: cifrar, decifrar ou sair
+# - possibilitar o usuário realizar quantas operações desejar [incompleta]
 # - ...
 
 def gerar_chave(texto_chave:str):
@@ -35,15 +33,50 @@ def cifrar_vigenere(text:str, chave:list):
 
     return texto_cifrado
 
-texto_de_origem = 'helloworld'
-chave_em_texto = 'kiosptz'
-chave_em_decimal = gerar_chave(chave_em_texto)
+def solicitar_escolha():
+    print('\n-----------------------------------------------')
+    print('---------------Cifra de Vigenère---------------')
+    print(' [1] - Criptografar   [2] - Descriptografar\n')
+    print(' [-]---------------   [4] - Sair            ')
+    print('-----------------------------------------------')
 
-#print(gerar_chave(chave_em_texto))
-print('Texto de origem:', texto_de_origem)
-print('Chave utilizada:', chave_em_texto, '-', end="")
-print('', *chave_em_decimal, '', sep=" | ")
-print('  Texto cifrado:', cifrar_vigenere(texto_de_origem, chave_em_decimal))
+    escolha = int(input('Digite sua escolha: '))
+    return escolha
+
+def validar_string(string, acao):
+    if len(string) > 128:
+        print(f'A string ultrapassou em ${len(string) - 128} caracteres o limite de 128. Tente novamente.')
+
+        string = input('Texto: ')
+
+        while(len(string) > 128):
+            print(f'Forneça um texto de até 128 caracteres para ser ${acao}.')
+            string = input('Texto: ')
+
+    return string
+
+escolha = solicitar_escolha()
+
+while(escolha != 4):
+    if(escolha == 1):
+        print('Forneça um texto de até 128 caracteres para ser cifrado.')
+        texto_de_origem = input('Texto: ')
+
+        print('\nForneça uma chave para ser utilizada.')
+        chave_em_texto = input('Chave: ')
+        chave_em_inteiros = gerar_chave(chave_em_texto)
+
+        texto_cifrado = cifrar_vigenere(texto_de_origem, chave_em_inteiros)
+
+        print(f'\nTexto de origem: {texto_de_origem}')
+        print(f'Chave utilizada: {chave_em_texto} - ', end="")
+        print(f'', *chave_em_inteiros, '', sep=" | ")
+        print('-----------------------------------------------')
+        print(f'Texto cifrado: {texto_cifrado}')
+        print('-----------------------------------------------')
+
+    escolha = solicitar_escolha()
+
 
 
 
